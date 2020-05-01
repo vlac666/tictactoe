56842dabc06e218ea92611b76a6cd0c84a4a6366
vlac666 <calvin.lepard@gmail.com>
2020-05-01 13:54:58 +0100

    Using loops to draw board

--- src/Board.js
+++ src/Board.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Square } from "./Square";
 
 export class Board extends React.Component {
-   renderSquare(i) {
+  renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
@@ -13,25 +13,23 @@ export class Board extends React.Component {
     );
   }
 
+  renderCols(rowNumber) {
+    const cols = [];
+    for (let c = 0; c < 3; c++) {
+      cols.push(this.renderSquare(rowNumber * 3 + c));
+    }
+    return cols;
+  }
+
+  renderRows() {
+    const rows = [];
+    for (let r = 0; r < 3; r++) {
+      rows.push(<div className="board-row">{this.renderCols(r)}</div>);
+    }
+    return rows;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.renderRows()}</div>;
   }
 }
