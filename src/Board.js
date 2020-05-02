c743e1369b1cc0ab72228a62885cec740cdf45c7
vlac666 <calvin.lepard@gmail.com>
2020-05-02 14:50:07 +0100

    refactored Board to  function

--- src/Board.js
+++ src/Board.js
@@ -5,16 +5,14 @@ import "./Board.css";
 export const ROW_COUNT = 3;
 export const COL_COUNT = 3;
 
-export class Board extends React.Component {
+export class xxxBoard extends React.Component {
   renderSquare(i) {
     return (
       <Square
         key={"sqaure" + i}
         isWinningLine={this.props.winningLine.includes(i)}
         value={this.props.squares[i]}
-        onClick={() => {
-          this.props.onClick(i);
-        }}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -41,5 +39,40 @@ export class Board extends React.Component {
 
   render() {
     return <div>{this.renderRows()}</div>;
+  }
+}
+
+export function Board({ squares, winningLine, onClick }) {
+  return <div>{renderRows()}</div>;
+
+  function renderRows() {
+    const rows = [];
+    for (let r = 0; r < ROW_COUNT; r++) {
+      rows.push(
+        <div key={"row" + r} className="board-row">
+          {renderCols(r)}
+        </div>
+      );
+    }
+    return rows;
+  }
+
+  function renderCols(rowNumber) {
+    const cols = [];
+    for (let c = 0; c < COL_COUNT; c++) {
+      cols.push(renderSquare(rowNumber * COL_COUNT + c));
+    }
+    return cols;
+  }
+
+  function renderSquare(i) {
+    return (
+      <Square
+        key={"sqaure" + i}
+        isWinningLine={winningLine.includes(i)}
+        value={squares[i]}
+        onClick={() => onClick(i)}
+      />
+    );
   }
 }
