6a7ba936696088540fb15acbad5e7073fb9efc37
vlac666 <calvin.lepard@gmail.com>
2020-05-08 11:46:16 +0100

    simplified tests

--- src/test/Game.spec.js
+++ src/test/Game.spec.js
@@ -6,53 +6,24 @@ describe("whosTurn", () => {
     test.todo("We should always check our params");
   });
 
-  describe("whosTurn(Odd Number)", () => {
-    it('should return "O" (oohh) when called with 1', () => {
-      assert.equal(whosTurn(1), "O");
-    });
-    it('should return "O" (oohh) when called with 9', () => {
-      assert.equal(whosTurn(1), "O");
-    });
-  });
-
-  describe("whosTurn(Even Number)", () => {
-    it('should return "X" when called with 2', () => {
-      assert.equal(whosTurn(2), "X");
-    });
-    it('should return "X" when called with 8', () => {
-      assert.equal(whosTurn(8), "X");
-    });
-  });
-
   describe("whosTurn(0)", () => {
-    it('should return "X" when called with 2', () => {
+    it('should return "X" when called with move number 0 becasue X goes first', () => {
       assert.equal(whosTurn(2), "X");
     });
+  });
+
+  describe("whosTurn - based on even and odd move number", ()=> {
+    it.each`
+      numericValue | expected
+      ${1}         | ${"O"}
+      ${2}         | ${"X"}
+      ${999999}    | ${"O"}
+      ${100000}    | ${"X"}
+    `(
+      'should return "$expected" when number = $numericValue',
+      ({ numericValue, expected }) => {
+        expect(whosTurn(numericValue)).toBe(expected);
+      }
+    );
   });
 });
-
-/**
- * 
-    describe('toText() - 1,000,000 to 999,999,999', function () {
-        it('should return "one million" when value is 1,000,000', function () {
-            assert.equal(numberText.toText(1000000), "one million")
-        });
-        it('should return "eleven million and one" when value is 11,000,001', function () {
-            assert.equal(numberText.toText(11000001), "eleven million and one")
-        });
-    });
- * 
- * 
- * 
- *     describe('toText() - 1,000 to 999,999', function () {
-        it.each`
-        numericValue | expected
-        ${1000}        | ${'one thousand'}
-        ${11101}        | ${'eleven thousand one hundred and one'}
-        ${999999}        | ${'nine hundred and ninety nine thousand nine hundred and ninety nine'}
-     `('should return "$expected" when number = $numericValue', ({ numericValue, expected }) => {
-            expect(numberText.toText(numericValue)).toBe(expected);
-        });
-    });
-
- */
